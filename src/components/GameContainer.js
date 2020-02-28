b4bedfb79ae17fb3382c05268449f4c0a0c730c3
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-02-28 10:41:03 -0800

    Added dummy images on the page

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
-//import ImageList from './ImageList';
+import ImageList from './ImageList';
 //import './App.css';
 
 class GameContainer extends Component {
@@ -9,12 +9,14 @@ class GameContainer extends Component {
         clicked: false
     }
 
+
+
     render() {
         return (<Container>
             <div>
                 <h1 className="text-center">Some Text</h1>
                 <h3 className="text-center">
-                    Sub Text
+                    <ImageList />
                 </h3>
 
             </div>
