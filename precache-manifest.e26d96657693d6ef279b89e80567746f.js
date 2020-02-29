d20488eb143a2201ab9829cdf394bd16ef600758
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-02-29 14:46:16 -0800

    Updates

---
+++ precache-manifest.e26d96657693d6ef279b89e80567746f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f064f681d0465c2f9cb7374d297065",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "760b944734eba3083b0c",
+    "url": "/clicky-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69990b720b6341d6546d",
+    "url": "/clicky-game/static/js/2.209becb1.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/clicky-game/static/js/2.209becb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760b944734eba3083b0c",
+    "url": "/clicky-game/static/js/main.b13cb1bf.chunk.js"
+  },
+  {
+    "revision": "c009a0a1e3686787466f",
+    "url": "/clicky-game/static/js/runtime-main.4231084e.js"
+  }
+]);
