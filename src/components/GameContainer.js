3a15e17a251e6f9b679bdd9dd479621cc75fca6b
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-02-28 21:35:16 -0800

    Added score in state; built logic to set score

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -6,17 +6,39 @@ import ImageList from './ImageList';
 
 class GameContainer extends Component {
     state = {
-        clicked: false
+        score: 0,
+        highScore: 0,
+        clickedArr: []
     }
 
+    handleImageClick = event => {
+        let score = this.state.score;
+        let highScore = this.state.highScore;
+        let newStateArray = this.state.clickedArr.slice();
 
+        const imgId = event.target.attributes.getNamedItem("data-id").value;
+        if (this.state.clickedArr.includes(imgId)) {
+            this.setState({
+                score: 0,
+                highScore: highScore,
+                clickedArr: []
+            });
+        } else {
+            newStateArray.push(imgId);
+            this.setState({
+                score: score + 1,
+                highScore: (highScore === score) ? highScore + 1 : highScore,
+                clickedArr: newStateArray
+            });
+        }
+    }
 
     render() {
         return (<Container>
             <div>
                 <h1 className="text-center">Some Text</h1>
                 <h3 className="text-center">
-                    <ImageList />
+                    <ImageList handleImageClick={this.handleImageClick} />
                 </h3>
 
             </div>
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Row, Col } from 'react-bootstrap';
 
-function ImageList() {
+function ImageList({ handleImageClick }) {
     return (
         <Row>
-            <Col><img src="https://media.giphy.com/media/TXJiSN8vCERuE/giphy.gif" /></Col>
-            <Col><img src="https://media.giphy.com/media/vcRW4hl1VvyCc/giphy.gif" /></Col>
-            <Col><img src="https://media.giphy.com/media/Gdyhq5vM5X4ly/giphy.gif" /></Col>
+            <Col><img src="https://media.giphy.com/media/TXJiSN8vCERuE/giphy.gif" data-id="1" onClick={handleImageClick} /></Col>
+            <Col><img src="https://media.giphy.com/media/vcRW4hl1VvyCc/giphy.gif" data-id="2" onClick={handleImageClick} /></Col>
+            <Col><img src="https://media.giphy.com/media/WEBiysleGJC6Y/giphy.gif" data-id="3" onClick={handleImageClick} /></Col>
+            <Col><img src="https://media.giphy.com/media/Gdyhq5vM5X4ly/giphy.gif" data-id="4" onClick={handleImageClick} /></Col>
+            <Col><img src="https://media.giphy.com/media/HuGCwDXj4nQnS/giphy.gif" data-id="5" onClick={handleImageClick} /></Col>
+
         </Row>
     );
 }
