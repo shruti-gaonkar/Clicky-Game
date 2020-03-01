be66f58fa735b993489ee611e6290801f6d55b26
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-03-01 01:45:18 -0800

    Added Card for each image

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 
 function ImageContainer({ handleImageClick, images }) {
     return (
         <Row>
             {images.map((value, index) => (
-                <Col key={value}><img src={`https://media.giphy.com/media/${value}/giphy.gif`} data-name={value} onClick={handleImageClick} width="200" height="200" /></Col>
+                <Col key={value} className="mt-3">
+                    <Card>
+                        <Card.Body>
+                            <img src={`https://media.giphy.com/media/${value}/giphy.gif`} data-name={value} onClick={handleImageClick} width="200" height="200" />
+                        </Card.Body>
+                    </Card>
+                </Col>
             ))}
         </Row >
     );
